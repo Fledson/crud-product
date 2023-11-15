6307053ef9d1f4d3e5eba90d056aef391d72fa9e
Fledson Henrique <fledsonhenrique@gmail.com>
2023-11-15 14:47:43 -0300

    :sparkles: feat: implementando update e delete do product

--- src/main/java/com/fledson/liveCoding/controllers/ProductController.java
+++ src/main/java/com/fledson/liveCoding/controllers/ProductController.java
@@ -3,10 +3,16 @@ package com.fledson.liveCoding.controllers;
 import com.fledson.liveCoding.domain.product.Product;
 import com.fledson.liveCoding.domain.product.ProductRepository;
 import com.fledson.liveCoding.domain.product.RequestProductDTO;
+import com.fledson.liveCoding.domain.product.RequestUpdateProductDTO;
+import jakarta.transaction.TransactionScoped;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -24,8 +30,22 @@ public class ProductController {
     @PostMapping
     public ResponseEntity registerProduct(@RequestBody @Valid RequestProductDTO data) {
         Product newProduct = new Product(data);
-        repository.save(newProduct);
+        newProduct = repository.save(newProduct);
         return ResponseEntity.ok(newProduct);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestUpdateProductDTO data) {
+        Product product = repository.findById(data.id()).orElseThrow( () -> new RuntimeException("Produto não existe"));
+        BeanUtils.copyProperties(data, product, "id");
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
