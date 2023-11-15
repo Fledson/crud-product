30fbd8499175af2d092c13e0e6ae9682c246b58f
Fledson Henrique <fledsonhenrique@gmail.com>
2023-11-15 15:04:37 -0300

    :recycle: refactor: criação de listagem de itens ativos

--- src/main/java/com/fledson/liveCoding/domain/product/ProductRepository.java
+++ src/main/java/com/fledson/liveCoding/domain/product/ProductRepository.java
@@ -2,5 +2,10 @@ package com.fledson.liveCoding.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+    List<Product> findAllByActiveTrue();
+
 }
